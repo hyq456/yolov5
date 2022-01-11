60e72d1cf9de8bb2f0691f1fe150128a3bb238f6
hyq <544608734@qq.com>
2022-01-11 17:14:16 +0800

    Revert "bifpn_eca.yaml modify"
    This reverts commit c7429e9fb154ab0570cf67205188bdc1246e1328.

--- models/common.py
+++ models/common.py
@@ -1264,17 +1264,3 @@ class eca_layer(nn.Module):
         x=x*y.expand_as(x)
 
         return x * y.expand_as(x)
-
-class Conv_hswish(nn.Module):
-    # Standard convolution
-    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
-        super().__init__()
-        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
-        self.bn = nn.BatchNorm2d(c2)
-        self.act = nn.Hardswish if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
-
-    def forward(self, x):
-        return self.act(self.bn(self.conv(x)))
-
-    def forward_fuse(self, x):
-        return self.act(self.conv(x))
