786ad12a35df215779ab9892d59bb0259d99c0e3
hyq <544608734@qq.com>
2022-03-11 14:47:48 +0800

    固定种子

--- utils/general.py
+++ utils/general.py
@@ -174,6 +174,7 @@ def init_seeds(seed=0):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
     cudnn.benchmark, cudnn.deterministic = (False, True) if seed == 0 else (True, False)
 
 
