1ad8ddc454855340988768938b3a1fc56a1e048a
hyq <544608734@qq.com>
2022-03-21 15:09:17 +0800

    c3_lka

--- models/common.py
+++ models/common.py
@@ -1147,6 +1147,23 @@ class lka(nn.Module):
         x = x + shorcut
         return x
 
+#---------------C3LKA--------
+class C3_LKA(nn.Module):
+    # C3 Bottleneck with 3 convolutions
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super(C3_LKA, self).__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.lka = lka(c1)
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c1, c_, 1, 1)
+        self.cv3 = Conv(2 * c_, c2, 1)  # act=FReLU(c2)
+        self.m = nn.Sequential(*[Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)])
+
+    def forward(self, x):
+        out = torch.cat((self.m(self.cv1(x)), self.cv2(self.lka(x))), dim=1)
+        out = self.cv3(out)
+        return out
+
 
 #---------------C3GC--------
 class C3_GC(nn.Module):
